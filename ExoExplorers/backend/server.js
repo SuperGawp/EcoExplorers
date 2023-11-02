a22d12e434f98a0de0c3675ad72a70a48de0940d
SuperGawp <aryanchandra2002@hotmail.com>
2023-11-01 21:29:13 -0600

    added calculator functionality

--- ExoExplorers/backend/server.js
+++ ExoExplorers/backend/server.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Import the CORS middleware
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors()); // Use the CORS middleware to allow cross-origin requests
 
 app.post('/calculate-emissions', (req, res) => {
   const { transportMode, distance, energySource, energyConsumption } = req.body;
 
-  // Implement the emissions calculation logic here
+  // Define emissions factors (g CO2 per unit)
+  const emissionsFactors = {
+    car: 120, // g CO2 per mile
+    bike: 0,  // g CO2 per mile
+    electricity: 50, // g CO2 per kWh
+  };
 
-  res.json({ totalEmissions });
+  let totalEmissions = 0;
+
+  if (transportMode in emissionsFactors) {
+    if (transportMode === 'electricity') {
+      totalEmissions = emissionsFactors[transportMode] * energyConsumption;
+    } else {
+      totalEmissions = emissionsFactors[transportMode] * distance;
+    }
+
+    res.json({ totalEmissions });
+  } else {
+    res.status(400).json({ error: 'Invalid transport mode' });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
--- ExoExplorers/frontend/App.js
+++ ExoExplorers/frontend/App.js
@@ -1,54 +1,113 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button, Dimensions } from 'react-native';
 
-export default function App() {
-  const [transportMode, setTransportMode] = useState('');
-  const [distance, setDistance] = useState('');
-  const [energySource, setEnergySource] = useState('');
-  const [energyConsumption, setEnergyConsumption] = useState('');
-  const [totalEmissions, setTotalEmissions] = useState(null);
+const window = Dimensions.get('window');
 
-  const calculateEmissions = () => {
-    // Implement the logic to calculate emissions here and update totalEmissions state.
-  };
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>ExoExplorers</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Transportation Mode"
-        value={transportMode}
-        onChangeText={setTransportMode}
-      />
-      {/* Add input fields for distance, energy source, and energy consumption */}
-      <Button title="Calculate Emissions" onPress={calculateEmissions} />
-      <Text style={styles.resultText}>Total Emissions: {totalEmissions}</Text>
-    </View>
-  );
-}
+const calcFontSize = (percent) => (window.height * percent) / 100;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
-    padding: 16, // Adjust padding for iPhone layout
+    alignItems: 'center',
+    width: window.width,
+    height: window.height,
+    padding: 16,
   },
-  title: {
-    fontSize: 20,
-    marginBottom: 20, // Adjust spacing for iPhone layout
+  header: {
+    fontSize: calcFontSize(4),
+    marginBottom: calcFontSize(2),
   },
   input: {
+    width: '90%',
+    height: calcFontSize(5),
+    borderColor: 'gray',
     borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 8,
-    marginBottom: 12, // Adjust spacing for iPhone layout
-    width: '100%', // Ensure input field takes the full width
+    marginBottom: calcFontSize(2),
+    paddingLeft: 8,
   },
-  resultText: {
-    fontSize: 18,
-    marginTop: 20, // Adjust spacing for iPhone layout
-  },
+  result: {
+    fontSize: calcFontSize(3),
+    marginTop: calcFontSize(2),
+    fontWeight: 'bold',
+  }
 });
+
+export default function App() {
+  const [transportation, setTransportation] = useState('');
+  const [energy, setEnergy] = useState('');
+  const [diet, setDiet] = useState('');
+  const [airTravel, setAirTravel] = useState('');
+  const [bikingWalking, setBikingWalking] = useState('');
+  const [treePlanting, setTreePlanting] = useState('');
+  const [gardening, setGardening] = useState('');
+  const [carbonFootprint, setCarbonFootprint] = useState(null);
+
+  const calculateCarbonFootprint = () => {
+    // Calculate the carbon footprint based on user input.
+    const transportationEmissions = parseFloat(transportation) * 0.80467; // 1 mile = 1.60934 km
+    const energyEmissions = parseFloat(energy) * 2;
+    const dietEmissions = parseFloat(diet) * 1.5;
+    const airTravelEmissions = parseFloat(airTravel) * 10;
+    const bikingWalkingEmissions = parseFloat(bikingWalking) * 0.2;
+    const treePlantingEmissions = parseFloat(treePlanting) * -0.5;
+    const gardeningEmissions = parseFloat(gardening) * 0.1;
+    const totalFootprint = transportationEmissions + energyEmissions + dietEmissions + airTravelEmissions + bikingWalkingEmissions + treePlantingEmissions + gardeningEmissions;
+
+    setCarbonFootprint(totalFootprint.toFixed(2));
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>EcoExplorers</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Kilometers driven per week"
+        onChangeText={(text) => setTransportation(text)}
+        keyboardType="default"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Hours of TV per day"
+        onChangeText={(text) => setEnergy(text)}
+        keyboardType="default"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Number of burgers per week"
+        onChangeText={(text) => setDiet(text)}
+        keyboardType="default"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Number of flights per year"
+        onChangeText={(text) => setAirTravel(text)}
+        keyboardType="default"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Hours of biking and walking per week"
+        onChangeText={(text) => setBikingWalking(text)}
+        keyboardType="default"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Number of trees planted"
+        onChangeText={(text) => setTreePlanting(text)}
+        keyboardType="default"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Hours spent gardening per week"
+        onChangeText={(text) => setGardening(text)}
+        keyboardType="default"
+      />
+      <Button title="Calculate" onPress={calculateCarbonFootprint} />
+      {carbonFootprint !== null && (
+        <Text style={[styles.result]}>
+          Your Carbon Footprint: {carbonFootprint} kg CO2e
+        </Text>
+      )}
+    </View>
+  );
+}
